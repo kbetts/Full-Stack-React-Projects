1a66946fccad8ab38004498762ef406468c4604d
Kellen Betts <kellen.betts@gmail.com>
2018-08-10 11:53:24 -0700

    experimenting with live server

--- Chapter02/mern-simplesetup/client/HelloWorld.js
+++ Chapter02/mern-simplesetup/client/HelloWorld.js
@@ -6,6 +6,11 @@ class HelloWorld extends Component {
     return (
         <div>
           <h1>Success!</h1>
+          <h2>Success!</h2>
+          <h3>Success!</h3>
+          <h4>Success!</h4>
+          <h5>Success!</h5>
+          <h6>Success!</h6>
         </div>
       )
   }
